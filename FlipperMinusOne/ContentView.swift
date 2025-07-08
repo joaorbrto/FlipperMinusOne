dc788c3fc20947ff4ff777214c298b1941a4d9f0
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2025-07-08 09:26:57 -0300

    Initial Commit

---
+++ FlipperMinusOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlipperMinusOne
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 08/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlipperMinusOne/FlipperMinusOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipperMinusOneApp.swift
+//  FlipperMinusOne
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FlipperMinusOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
