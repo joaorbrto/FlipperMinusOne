ac5d84077a13aa5bfd6435911d4d2c1b76331367
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2025-07-10 09:39:24 -0300

    MQTT Manager implemented

---
+++ FlipperMinusOne/Model/FlipperData.swift
@@ -0,0 +1,12 @@
+//
+//  FlipperData.swift
+//  FlipperMinusOne
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 10/07/25.
+//
+
+import Foundation
+
+struct FlipperData: Codable {
+    let carga: Int
+}
