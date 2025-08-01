3ad589798f73b55acaf2b9fb1826851a9c8f0881
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2025-08-01 20:15:24 -0300

    .

---
+++ FlipperMinusOne/View/ConnectionForm.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectionForm.swift
+//  FlipperMinusOne
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 29/07/25.
+//
+
+import SwiftUI
+
+struct ConnectionForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConnectionForm()
+}
